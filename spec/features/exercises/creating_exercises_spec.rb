2c58e1501cc8077624396858227ea76ca06736e7
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-10 19:52:39 +0700

    added exercise model and _form partial

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -14,8 +14,8 @@ RSpec.feature 'Creating Exercises' do
     expect(page).to have_link("Back")
 
     fill_in 'Duration', with: 70
-    fill_in 'Workout Details', with: 'Weight lifting'
-    fill_in 'Actvity date', with: '2016-07-26'
+    fill_in 'Workout details', with: 'Weight lifting'
+    fill_in 'Activity date', with: '2016-07-26'
     click_button('Create Exercise')
 
     expect(page).to have_content('Exercise has been created')
