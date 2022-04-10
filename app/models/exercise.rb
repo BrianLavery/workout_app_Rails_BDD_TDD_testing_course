e1c9342ddd759d4f2d3449f0a81bdf012a36c703
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-10 20:13:58 +0700

    created exercise validators

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,10 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+  # Attribute aliases
+  alias_attribute :workout_details, :workout
+  alias_attribute :activity_date, :workout_date
+
+  validates :duration_in_min, :workout_details, :activity_date, presence: true
+  validates :duration_in_min, numericality: { greater_than: 0.0 }
 end
