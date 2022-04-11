8bedd98ca59b915753b15db3919859c3c517deb4
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-04-11 18:37:33 +0700

    implement searching for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,33 @@ class User < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
+
+    def self.search_by_name(name)
+    names_array = name.split(' ')
+
+    if names_array.size == 1
+      where('first_name LIKE ? or last_name LIKE ?',
+        "%#{names_array[0]}%", "%#{names_array[0]}%").order(:first_name)
+    else
+      where('first_name LIKE ? or first_name LIKE ? or last_name LIKE ?
+        or last_name LIKE ?', "%#{names_array[0]}%",
+        "%#{names_array[1]}%", "%#{names_array[0]}%",
+        "%#{names_array[1]}%").order(:first_name)
+    end
+  end
+
+  # def follows_or_same?(new_friend)
+  #   friendships.map(&:friend).include?(new_friend) || self == new_friend
+  # end
+
+  # def current_friendship(friend)
+  #   friendships.where(friend: friend).first
+  # end
+
+  # private
+
+  # def create_chatroom
+  #   hyphenated_username = self.full_name.split.join('-')
+  #   Room.create(name: hyphenated_username, user_id: self.id)
+  # end
 end
